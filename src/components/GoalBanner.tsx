a3f0f2bb30b6f3a80de6c342e93f46530d173cdd
DarkFalc0n <pratyayroy20@gmail.com>
2023-10-25 21:46:31 +0530

    special mention corrected

--- src/components/GoalBanner.tsx
+++ src/components/GoalBanner.tsx
@@ -5,7 +5,7 @@ const GoalBanner = () => {
     <div className="my-6 px-24 py-12 text-3xl font-bold text-center bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 text-transparent bg-clip-text">
       Congratulations to the top 80, we made it to Tier-1 !!
       <div className="text-lg font-semibold mt-5">
-        Special Mention: Subhraneel Bera (Completed 15 badges on 25th October)
+        Special Mention: Subhraneel Bera (Earned 15 badges on 25th October)
       </div>
     </div>
   );
