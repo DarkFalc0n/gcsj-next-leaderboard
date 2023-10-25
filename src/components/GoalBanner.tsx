0939f2bba40321bbfbf4f4ced3888489f82d9de1
DarkFalc0n <pratyayroy20@gmail.com>
2023-10-25 21:32:11 +0530

    corrected

--- src/components/GoalBanner.tsx
+++ src/components/GoalBanner.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const GoalBanner = () => {
   return (
     <div className="my-6 px-24 py-12 text-3xl font-bold text-center bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 text-transparent bg-clip-text">
-      Congratulations to the top 80, we made it to Tier-3 !!
+      Congratulations to the top 80, we made it to Tier-1 !!
     </div>
   );
 };
